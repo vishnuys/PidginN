69ea810fb1c6dd485d7d1ab8f20849ff3a9e0220
Chirag Vij <vijchirag.samrat10@gmail.com>
2019-09-07 21:43:53 +0530

    Add files via upload

---
+++ Client/client.java
@@ -0,0 +1,70 @@
+package client;
+
+import java.net.*; 
+import java.io.*;
+
+public class client {
+	private Socket socket            = null; 
+    private DataInputStream  input   = null; 
+    private DataOutputStream out     = null; 
+  
+    // constructor to put IP address and port 
+    public client(String address, int port) 
+    { 
+        // establish a connection 
+        try
+        { 
+            socket = new Socket(address, port); 
+            System.out.println("Connected"); 
+  
+            // takes input from terminal 
+            input  = new DataInputStream(System.in); 
+  
+            // sends output to the socket 
+            out    = new DataOutputStream(socket.getOutputStream()); 
+        } 
+        catch(UnknownHostException u) 
+        { 
+            System.out.println(u); 
+        } 
+        catch(IOException i) 
+        { 
+            System.out.println(i); 
+        } 
+  
+        // string to read message from input 
+        String line = ""; 
+  
+        // keep reading until "Over" is input 
+        while (!line.equals("Over")) 
+        { 
+            try
+            { 
+                line = input.readLine(); 
+                out.writeUTF(line); 
+            } 
+            catch(IOException i) 
+            { 
+                System.out.println(i); 
+            } 
+        } 
+  
+        // close the connection 
+        try
+        { 
+            input.close(); 
+            out.close(); 
+            socket.close(); 
+        } 
+        catch(IOException i) 
+        { 
+            System.out.println(i); 
+        } 
+    } 
+  
+    public static void main(String args[]) 
+    { 
+        client client1 = new client("127.0.0.1", 5000); 
+    } 
+
+}
