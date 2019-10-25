1544f9112ed81c4a01a26062b11ee5a3a5723c10
Vishnu Y S <ysv8152@gmail.com>
2019-10-25 17:43:35 +0530

    Chat screen translation working

--- Server/pidgin-server/src/main/java/OOAD/RestHandler.java
+++ Server/pidgin-server/src/main/java/OOAD/RestHandler.java
@@ -11,32 +11,63 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.Translate.TranslateOption;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
+
 @RestController
 public class RestHandler {
-	
+
 	@Autowired
 	private UserService user;
-	
+
 	@RequestMapping("/users")
-    public List<UserTable> allUsers() {
-        return user.GetAllUsers();
-    }
-    
-    @RequestMapping(method=RequestMethod.POST, value="/addUser")
-    public void addUser(@RequestBody UserTable u) {
-        user.AddUser(u);
-    }
-    
-    @RequestMapping(method=RequestMethod.POST, value="/login")
-    public String login(@RequestParam("username") String uname, @RequestParam("password") String pwd) {
-        List<UserTable> users = user.GetAllUsers();
-        for(UserTable user:users) {
-        	System.out.println(uname + " " + user.getUsername().toString());
-        	System.out.println(pwd + " " + user.getPassword().toString());
-        	if(user.getUsername().equals(uname) && user.getPassword().equals(pwd)) {
-        		return "Success";
-        	}
-        }
-        return "Failed";
-    }
+	public List<UserTable> allUsers() {
+		return user.GetAllUsers();
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/addUser")
+	public void addUser(@RequestBody UserTable u) {
+		user.AddUser(u);
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/login")
+	public String login(@RequestParam("username") String uname, @RequestParam("password") String pwd) {
+		List<UserTable> users = user.GetAllUsers();
+		for (UserTable user : users) {
+			System.out.println(uname + " " + user.getUsername().toString());
+			System.out.println(pwd + " " + user.getPassword().toString());
+			if (user.getUsername().equals(uname) && user.getPassword().equals(pwd)) {
+				return "Success";
+			}
+		}
+		return "Failed";
+	}
+
+	public String getUserLang(String username) {
+		String lang = "";
+		List<UserTable> users = user.GetAllUsers();
+		for (UserTable user : users) {
+			if (user.getUsername().equals(username)) {
+				lang = user.getLanguage();
+				break;
+			}
+		}
+		return lang;
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/translate")
+	public String translate(@RequestParam("sender") String sender, @RequestParam("receiver") String receiver,
+			@RequestParam("text") String text) {
+		String senderLang = getUserLang(sender);
+		String receiverLang = getUserLang(receiver);
+		@SuppressWarnings("deprecation")
+		Translate translate = TranslateOptions.newBuilder().setApiKey("AIzaSyAiRYBhdm3LPeKeq-ClxWERz_vAMPcX2Rw").build()
+				.getService();
+		Translation translation = translate.translate(text, TranslateOption.sourceLanguage(senderLang),
+				TranslateOption.targetLanguage(receiverLang));
+		String translatedText = translation.getTranslatedText();
+		return translatedText;
+	}
 }
--- Server/pidgin-server/src/main/resources/static/js/chat.js
+++ Server/pidgin-server/src/main/resources/static/js/chat.js
@@ -2,125 +2,166 @@ var app = angular.module('OOAD', []);
 app
 		.controller(
 				'pidgin',
-				function($scope) {
+				[
+						'$scope',
+						'$http',
+						function($scope, $http) {
+							$scope.loading = true;
+							$scope.username = getCookie('username');
+							$scope.websocket = '';
+							$scope.connections = [
+									{
+										username : 'g',
+										name : 'Howard',
+										lastMessage : 'PLEASE COMPLETE IT SOON',
+										lastUpdated : '3 days ago',
+										chatMessages : [
+												{
+													sender : 'g',
+													receiver : 'm',
+													message : 'Esto debe hacerse de inmediato',
+													translatedMessage : 'This needs to be done immediately',
+												},
+												{
+													sender : 'm',
+													receiver : 'g',
+													message : "Yes, I'm on it. I'll send it as soon as I am done.",
+													translatedMessage : 'Sí, estoy en eso. Lo enviaré tan pronto como termine.'
+												} ]
+									},
+									{
+										username : 'n',
+										name : 'Barney',
+										lastMessage : 'PLEASE COMPLETE IT SOON',
+										lastUpdated : '4 days ago'
+									},
+									{
+										username : 'joey',
+										name : 'Joey',
+										lastMessage : 'PLEASE COMPLETE IT SOON',
+										lastUpdated : '5 days ago'
+									},
+									{
+										username : 'michael',
+										name : 'Michael',
+										lastMessage : 'PLEASE COMPLETE IT SOON',
+										lastUpdated : '6 days ago'
+									} ];
 
-					$scope.loading = true;
-					$scope.username = getCookie('username');
-					$scope.websocket = '';
-					$scope.connections = [
-							{
-								username : 'howard',
-								name : 'Howard',
-								lastMessage : 'PLEASE COMPLETE IT SOON',
-								lastUpdated : '3 days ago',
-								chatMessages : [
-										{
-											username : 'howard',
-											message : 'This needs to be done immediately',
-											translatedMessage : 'Esto debe hacerse de inmediato'
-										},
-										{
-											username : 'a',
-											message : "Yes, I'm on it. I'll send it as soon as I am done.",
-											translatedMessage : 'Sí, estoy en eso. Lo enviaré tan pronto como termine.'
-										} ]
-							}, {
-								username : 'barney',
-								name : 'Barney',
-								lastMessage : 'PLEASE COMPLETE IT SOON',
-								lastUpdated : '4 days ago'
-							}, {
-								username : 'joey',
-								name : 'Joey',
-								lastMessage : 'PLEASE COMPLETE IT SOON',
-								lastUpdated : '5 days ago'
-							}, {
-								username : 'michael',
-								name : 'Michael',
-								lastMessage : 'PLEASE COMPLETE IT SOON',
-								lastUpdated : '6 days ago'
-							} ];
+							$scope.currentUser = $scope.connections[0].username;
 
-					$scope.currentUser = $scope.connections[0].username;
-
-					function getCookie(cname) {
-						var name = cname + "=";
-						var ca = document.cookie.split(';');
-						for (var i = 0; i < ca.length; i++) {
-							var c = ca[i];
-							while (c.charAt(0) == ' ') {
-								c = c.substring(1);
+							function getCookie(cname) {
+								var name = cname + "=";
+								var ca = document.cookie.split(';');
+								for (var i = 0; i < ca.length; i++) {
+									var c = ca[i];
+									while (c.charAt(0) == ' ') {
+										c = c.substring(1);
+									}
+									if (c.indexOf(name) == 0) {
+										return c.substring(name.length,
+												c.length);
+									}
+								}
+								return "";
 							}
-							if (c.indexOf(name) == 0) {
-								return c.substring(name.length, c.length);
+
+							$scope.ready = function() {
+								if ($scope.username == '') {
+									location.href = '/login';
+								}
+								$scope.connect();
+							};
+
+							function setConnected(connected) {
+								$("#connect").prop("disabled", connected);
+								$("#disconnect").prop("disabled", !connected);
 							}
-						}
-						return "";
-					}
 
-					$scope.ready = function() {
-						if ($scope.username == '') {
-							location.href = '/login';
-						}
-						$scope.connect();
-					};
-
-					function setConnected(connected) {
-						$("#connect").prop("disabled", connected);
-						$("#disconnect").prop("disabled", !connected);
-					}
-
-					$scope.connect = function() {
-						$scope.websocket = new WebSocket(
-								'ws://localhost:8080/chat/' + $scope.username);
-						$scope.websocket.onmessage = function(data) {
-							addToPage(data.data);
-						}
-						setConnected(true);
-						$scope.loading = false;
-					}
-
-					function disconnect() {
-						if (ws != null) {
-							$scope.websocket.close();
-						}
-						setConnected(false);
-						console.log("Websocket is in disconnected state");
-					}
-
-					function sendData() {
-						var data = JSON.stringify({
-							'user' : $("#send-text").val()
-						})
-						$scope.websocket.send(data);
-					}
-
-					function addToPage(message) {
-						console.log(message);
-					}
-
-					$scope.getUserIndex = function(username) {
-						for ( var i in $scope.connections) {
-							if ($scope.connections[i].username == username) {
-								return i;
+							$scope.connect = function() {
+								$scope.websocket = new WebSocket(
+										'ws://localhost:8080/chat/'
+												+ $scope.username);
+								$scope.websocket.onmessage = function(data) {
+									addToPage(data.data);
+								}
+								setConnected(true);
+								$scope.loading = false;
 							}
-						}
-					};
 
-					$scope.sendMessage = function(msg) {
-						console.log($scope.messageToSend);
-						var data = {
-							username : $scope.username,
-							message : msg,
-							translatedMessage : 'Loading...'
-						}
-						var index = $scope.getUserIndex($scope.currentUser);
-						$scope.connections[index].chatMessages.push(data);
-						$('#send-text').val('');
-					}
+							function disconnect() {
+								if (ws != null) {
+									$scope.websocket.close();
+								}
+								setConnected(false);
+								console
+										.log("Websocket is in disconnected state");
+							}
 
-					$scope.print = function(msg) {
-						console.log(msg);
-					}
+							function sendData() {
+								var data = JSON.stringify({
+									'user' : $("#send-text").val()
+								})
+								$scope.websocket.send(data);
+							}
 
-				});
+							function addToPage(message) {
+								console.log(message);
+							}
+
+							$scope.getUserIndex = function(username) {
+								for ( var i in $scope.connections) {
+									if ($scope.connections[i].username == username) {
+										return i;
+									}
+								}
+							};
+
+							$scope.sendMessage = function(msg) {
+								$
+										.ajax({
+											url : '/translate',
+											type : 'POST',
+											data : {
+												'sender' : $scope.username,
+												'receiver' : $scope.currentUser,
+												'text' : msg
+											},
+											success : function(data) {
+												var data = {
+													sender : $scope.username,
+													receiver : $scope.currentUser,
+													message : msg,
+													translatedMessage : data
+												}
+												var index = $scope
+														.getUserIndex($scope.currentUser);
+												$scope.connections[index].chatMessages
+														.push(data);
+												$("#chatbox")
+														.animate(
+																{
+																	scrollTop : $(
+																			'#chatbox')
+																			.get(
+																					0).scrollHeight
+																}, 1000);
+												$('#send-text').val('');
+												$scope.$digest();
+											},
+											error : function(e) {
+												console.error(e)
+											}
+										});
+							}
+
+							$scope.print = function(msg) {
+								console.log(msg);
+							}
+
+							$scope.logout = function() {
+								document.cookie = "username=;expires=Thu; 01 Jan 1970"
+								location.href = '/login';
+							}
+
+						} ]);
