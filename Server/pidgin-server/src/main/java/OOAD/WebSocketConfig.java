ebae621df91982994a8405dbe3e82c3a1dfda1ad
Vishal Kushwaha <vishsinkush@gmail.com>
2019-11-25 00:31:03 +0530

    add message handling code

--- Server/pidgin-server/src/main/java/OOAD/WebSocketConfig.java
+++ Server/pidgin-server/src/main/java/OOAD/WebSocketConfig.java
@@ -1,19 +1,26 @@
 package OOAD;
 
+import java.security.Principal;
+import java.util.Map;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
 	
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/chatroom");
+		registry.addEndpoint("/chatroom")
 //		.withSockJS();
-//		.setHandshakeHandler(new UsernameHandshakeHandler());
+		.setHandshakeHandler(new UsernameHandshakeHandler());
 	}
 	
 	public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -21,11 +28,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
 			  .enableSimpleBroker("/topic","/queue");
 	}
 	
-//	private class UsernameHandshakeHandler extends DefaultHandshakeHandler {
-//		protected Principal determineUser(ServerHttpRequest request, WebSocketHandler wsHandler,
-//				Map<String, Object> attributes) {
-//			String username = "user-" + (int) (Math.random()*5 + 5);
-//			return new UsernamePasswordAuthenticationToken(username, null);
-//		}
-//	}
+	private class UsernameHandshakeHandler extends DefaultHandshakeHandler {
+		protected Principal determineUser(ServerHttpRequest request, WebSocketHandler wsHandler,
+				Map<String, Object> attributes) {
+			String username = (String) attributes.get("username");
+			return new UsernamePasswordAuthenticationToken(username, null);
+		}
+	}
 }
