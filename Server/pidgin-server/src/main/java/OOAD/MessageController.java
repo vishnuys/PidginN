a4156cb9fb20afde34a7c461c9e9dcc78fd45a82
Vishal Kushwaha <vishsinkush@gmail.com>
2019-11-25 12:24:50 +0530

    add send to annotation to broadcast message

--- Server/pidgin-server/src/main/java/OOAD/MessageController.java
+++ Server/pidgin-server/src/main/java/OOAD/MessageController.java
@@ -15,6 +15,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -77,6 +78,7 @@ public class MessageController {
 	private SimpMessagingTemplate simpMessagingTemplate;
 	
 	@MessageMapping("/chatroom")
+	@SendTo("/topic/chatroom")
 	public String broadcastToClients(String message, Principal principal) {
 		return message.toUpperCase() + " by user:" + principal.getName();
 	}
