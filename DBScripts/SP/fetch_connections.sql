05236ab94b823e5c0376a563e47689b1f1ce2eb7
Naveen <naveenbabu9595@gmail.com>
2019-11-25 19:41:42 +0530

    change in fetchmsg

--- DBScripts/SP/fetch_connections.sql
+++ DBScripts/SP/fetch_connections.sql
@@ -2,7 +2,6 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `fetch_connections`(
 IN inuserID INT
 )
 BEGIN
-
 	SELECT UserID,UserName,FirstName,LastName,PreferedLanguage,EmailID FROM UserDetails WHERE UserID IN (SELECT ConnectionUserID FROM UserConnections WHERE UserID=inuserID);
 
 END
